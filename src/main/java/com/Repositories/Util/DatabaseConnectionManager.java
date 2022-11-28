0f783265dc3c06d1f647803f50aa603ac1ed7250
Mathias <mathias.blak@gmail.com>
2022-11-28 09:50:47 +0100

    Connection

--- src/main/java/com/Repositories/Util/DatabaseConnectionManager.java
+++ src/main/java/com/Repositories/Util/DatabaseConnectionManager.java
@@ -1,4 +1,29 @@
 package com.Repositories.Util;
 
+import com.sun.jdi.connect.spi.Connection;
+
 public class DatabaseConnectionManager {
+  //Variabler
+  private static String hostname;
+  private static String username;
+  private static String password;
+  private static Connection connection;
+
+  //Metoder
+
+  public static Connection getConnection(){
+    hostname = "jdbc:mysql://.database.azure.com/BilAbonnementDB";
+    username = "Glennhansen";
+    password = "kalvin";
+
+    try {
+      connection = DriverManager.getConnection(hostname,username,password);
+
+    } catch (SQLException e) {
+      System.out.println("Kan ikke forbinde til database");
+      throw new RuntimeException(e);
+    }
+    System.out.println("Forbindelse til database godkendt!");
+    return connection;
+  }
 }
