0c7fd16427594d78eafdd3b75e173fa11f2f98be
haugaard1101 <s205434@student.dtu.dk>
2022-11-29 09:46:46 +0100

    getBil og getLejeAftale i SOUrepo

--- src/main/java/com/example/bilabonnement/Repositories/SOURepository.java
+++ src/main/java/com/example/bilabonnement/Repositories/SOURepository.java
@@ -1,7 +1,11 @@
 package com.example.bilabonnement.Repositories;
 
+import com.example.bilabonnement.Model.BilModel;
+import com.example.bilabonnement.Model.LejeAftaleModel;
 import com.example.bilabonnement.Model.SOUModel;
 import com.example.bilabonnement.Repositories.Util.DatabaseConnectionManager;
+import com.example.bilabonnement.Model.UdlejningsStatusEnum;
+import com.example.bilabonnement.Model.GearEnum;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -33,6 +37,68 @@ public class SOURepository {
             throw new RuntimeException(e);
         }
         return SOUListe;
+    }
+
+    public List<LejeAftaleModel> getLejeAftaleByRegistreringsNummer(int id){
+        List<LejeAftaleModel> LejeAftaleSOU = new ArrayList<>();
+
+        try {
+            PreparedStatement psts = connection.prepareStatement("SELECT * FROM LejeAftale where RegistreringsNummer = id");
+            ResultSet resultSet = psts.executeQuery();
+
+            while (resultSet.next()) {
+                LejeAftaleSOU.add(new LejeAftaleModel(
+                        resultSet.getString("Navn"),
+                        resultSet.getString("Adresse"),
+                        resultSet.getInt("Postnummer"),
+                        resultSet.getString("Kommune"),
+                        resultSet.getInt("TelefonNr"),
+                        resultSet.getString("CPR"),
+                        resultSet.getString("Email"),
+                        resultSet.getDate("LejeperiodeFra"),
+                        resultSet.getDate("LejeperiodeTil"),
+                        resultSet.getInt("Antal_Måneder"),
+                        resultSet.getString("Afhentningssted"),
+                        resultSet.getString("Afleveringssted"),
+                        resultSet.getInt("KM_ved_Afhentning"),
+                        resultSet.getInt("AftaleID"),
+                        resultSet.getInt("MaxKilometer"),
+                        resultSet.getInt("AktueltKørteKilometer"),
+                        resultSet.getInt("RegistreringsNummer")
+                ));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return LejeAftaleSOU;
+    }
+
+    public List<BilModel> getBilByRegistreringsNummer(int id){
+        List<BilModel> BilListe = new ArrayList<>();
+
+        try {
+            PreparedStatement psts = connection.prepareStatement("SELECT * FROM Biler where RegistreringsNummer = id");
+            ResultSet resultSet = psts.executeQuery();
+
+            while (resultSet.next()) {
+                BilListe.add(new BilModel(
+                        resultSet.getInt("RegistreringsNummer"),
+                        resultSet.getString("Stelnummer"),
+                        resultSet.getString("Mærke"),
+                        resultSet.getString("Model"),
+                        resultSet.getString("UdstyrsNiveau"),
+                        resultSet.getENUM("UdlejningsStatus"),
+                        resultSet.getENUM("Gear"),
+                        resultSet.getString("BrændstofType"),
+                        resultSet.getInt("KM_L"),
+                        resultSet.getInt("CO2_Udledning"),
+                        resultSet.getInt("Pris_pr_Måned")
+                ));
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return BilListe;
     }
 
 }
