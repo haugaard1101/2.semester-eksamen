a551ba28a1c7c8c587bb7ba484bc7b8a1955aa5f
Mathias <mathias.blak@gmail.com>
2022-12-01 11:08:35 +0100

    dataregistrering html

--- src/main/java/com/example/bilabonnement/Controller/LejeAftaleController.java
+++ src/main/java/com/example/bilabonnement/Controller/LejeAftaleController.java
@@ -2,11 +2,9 @@ package com.example.bilabonnement.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 
-import javax.servlet.http.HttpSession;
-
 public class LejeAftaleController {
-  @GetMapping("/Dataregistrering")
-  public String index(){
-    return "Random";
+  @GetMapping("/dataregistrering")
+  public String dataregistrering(){
+    return "dataregistrering";
   }
 }
