edaa919a95a482cba89f886f577b53abcd425b70
Mauf8 <marc32n@stud.kea.dk>
2022-12-05 15:07:58 +0100

    gået igang med at fikse dataregistreing html med bootstrap

--- src/main/java/com/example/bilabonnement/Model/SQLUpdated.sql
+++ src/main/java/com/example/bilabonnement/Model/SQLUpdated.sql
@@ -96,3 +96,8 @@ values ('HF73287','Marcus Buhl', 'Hvidovrevej 23', 2520, 'Hvidovre', 25230299, '
         '2021-11-12', '2022-04-12', 6, 'Vanløse bilværksted', 'Vanløse bilværksted', 34000, 1500, 2000);
 INSERT INTO skader (RegistreringsNummer, SkadeNavn, SkadePris)
 values ('HF73287', 'skade', 1000)
+
+
+Insert into Biler (RegistreringsNummer, Stelnummer, Mærke, Model, UdstyrsNiveau, UdlejningsStatus, Gear, BrændstofType, KmL, CO2_Udledning,
+                   PrisPrMåned)
+values ('GG66666',12345789, 'Mclaren', 'hurtigbil', 'Premium', 'AFLEVERET', 'AUTOMATISK', 'benzin', 45, 70, 99999);
