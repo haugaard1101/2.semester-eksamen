89c22589476bfae8f4e5b6f1dcfe484225cc4aa9
Benjamin J N <bjmn93@live.dk>
2022-12-09 09:31:49 +0100

    Fixed seledigebiler

--- src/main/java/com/example/bilabonnement/Controller/Dataregistreringscontroller.java
+++ src/main/java/com/example/bilabonnement/Controller/Dataregistreringscontroller.java
@@ -12,97 +12,105 @@ import java.util.Objects;
 
 @Controller
 public class Dataregistreringscontroller {
-//Instantiering af dataregistreringsservice, så den kan kaldes op i controlleren
+  //Instantiering af dataregistreringsservice, så den kan kaldes op i controlleren
   DataregistreringsService dataregistreringsService = new DataregistreringsService();
-//Getmapping til fejlsiden for data specifikt
+
+  //Getmapping til fejlsiden for data specifikt
   @GetMapping("/fejlsidedata")
-  public String fejlsidedata(){
+  public String fejlsidedata() {
     return "dataregistrering/fejlsidedata";
   }
+
   //Getmapping til standard dataregistreringssiden
   @GetMapping("/dataregistrering")
-  public String dataregistrering(){
+  public String dataregistrering() {
     return "dataregistrering/dataregistrering";
   }
 
   @GetMapping("/lejeaftalefundet")
-  public String lejeaftalefundet(){
+  public String lejeaftalefundet() {
     return "dataregistrering/lejeaftalefundet";
   }
+
   @GetMapping("/seogredigerlejeaftaler")
-  public String seogredigerlejeaftaler(){
+  public String seogredigerlejeaftaler() {
     return "dataregistrering/seogredigerlejeaftaler";
   }
 
   @GetMapping("/opdaterlejeaftale")
-  public String opdaterlejeaftale(){
+  public String opdaterlejeaftale() {
     return "dataregistrering/opdaterlejeaftale";
   }
+
   @GetMapping("/lejeaftaleliste")
-  public String lejeaftaleliste(Model model){
+  public String lejeaftaleliste(Model model) {
     model.addAttribute("LejeAftale", dataregistreringsService.getAlleLejeAftaler());
     return "dataregistrering/lejeaftaleliste";
   }
+
   @GetMapping("/updatesinglevalue")
-  public String updatesinglevalue(){
+  public String updatesinglevalue() {
     return "dataregistrering/updatesinglevalue";
   }
+
   @PostMapping("/deleteLegeAftale")
-  public String deleteLegeAftale(WebRequest request){
-    try{
+  public String deleteLegeAftale(WebRequest request) {
+    try {
       dataregistreringsService.deleteLejeAftale(request.getParameter("RegistreringsNummer"));
-    }catch (Exception e){
+    } catch (Exception e) {
       return "redirect:fejlsidedata";
     }
     return "redirect:lejeaftaleliste";
   }
+
   @PostMapping("/updatingsinglevalue")
-  public String updatingsinglevalue(WebRequest request){
-    try{
+  public String updatingsinglevalue(WebRequest request) {
+    try {
       int x = Integer.parseInt(Objects.requireNonNull(request.getParameter("KmVedIndlevering")));
-      dataregistreringsService.updateSingleValue(request.getParameter("RegistreringsNummer"),x);
-    }catch (Exception e){
+      dataregistreringsService.updateSingleValue(request.getParameter("RegistreringsNummer"), x);
+    } catch (Exception e) {
       return "redirect:fejlsidedata";
     }
     return "dataregistrering/updatesinglevalue";
   }
 
   @PostMapping("/lejeaftalefundet")
-  public String findLejeaftale(WebRequest request,Model model){
-    try{
+  public String findLejeaftale(WebRequest request, Model model) {
+    try {
       model.addAttribute("LejeAftale", dataregistreringsService.getLejeAftaleById(request.getParameter("RegistreringsNummer")));
-    }catch (Exception e){
+    } catch (Exception e) {
       return "redirect:fejlsidedata";
     }
     return "dataregistrering/lejeaftalefundet";
   }
+
   @PostMapping("/lejeaftaleliste")
-  public String sendlejekontrakt(WebRequest request,Model model){
-    try{
-      dataregistreringsService.createLejeAftale(request,"create");
+  public String sendlejekontrakt(WebRequest request, Model model) {
+    try {
+      dataregistreringsService.createLejeAftale(request, "create");
       model.addAttribute("LejeAftale", dataregistreringsService.getAlleLejeAftaler());
 
-    }catch (Exception e){
+    } catch (Exception e) {
       return "redirect:fejlsidedata";
     }
     return "dataregistrering/lejeaftaleliste";
   }
+
   @PostMapping("/opdaterlejeaftale")
-  public String opdaterlejeaftale(WebRequest request,Model model){
-    try{
-      dataregistreringsService.createLejeAftale(request,"");
+  public String opdaterlejeaftale(WebRequest request, Model model) {
+    try {
+      dataregistreringsService.createLejeAftale(request, "");
       model.addAttribute("LejeAftale", dataregistreringsService.getAlleLejeAftaler());
-    }catch (Exception e){
+    } catch (Exception e) {
       return "redirect:fejlsidedata";
     }
 
     return "dataregistrering/opdaterlejeaftale";
   }
 
-//  @GetMapping("/seledigebiler")
-//  public String showRentedCars(Model model){
-//    model.addAttribute("RentedCars", service.getAllRentedCars());
-//    model.addAttribute("amountOfCars", service.getAllRentedCars().size());
-//    return "/forretningsudvikling/forretningsudvikling";
-//  }
+  @GetMapping("/seledigebiler")
+  public String getAllLedigeBiler(Model model) {
+    model.addAttribute("LedigeBiler", dataregistreringsService.getAllLedigeBiler());
+    return "dataregistrering/seledigebiler";
+  }
 }
